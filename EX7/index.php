15a61ff18bd8213275bf464a1ab17ba598257e16
Zawatsuka <57181738+Zawatsuka@users.noreply.github.com>
2020-12-09 11:24:52 +0100

    ajout de l'exo7 et 8

---
+++ EX7/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exo7</title>
+</head>
+
+<body>
+    <?php $lastName = 'Hubert'; 
+        $firstName = 'Bonisseur De La Bath';
+        $age = 48; ?>
+    <p><?php echo 'Bonjour '.$lastName.' '.$firstName.', vous avez bien '.$age.' ans.'?></p>
+</body>
+
+</html>
---
+++ EX8/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exo8</title>
+</head>
+<body>
+    <?php $number= 3 + 4 ; ?>
+    <p><?php echo $number; ?></p>
+      <?php  $number = 5*20 ; ?>
+      <p><?php echo $number; ?></p>
+       <?php $number = 45/5; ?>
+       <p><?php echo $number ; ?></p>
+</body>
+</html>
