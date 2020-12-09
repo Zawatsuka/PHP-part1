92c09a1359a6343531401c968cacb844f8cd8cea
Zawatsuka <57181738+Zawatsuka@users.noreply.github.com>
2020-12-09 11:05:57 +0100

    ajout de l'exo6

---
+++ EX6/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EXO6</title>
+</head>
+<body>
+    <?php $name = ' Hubert Bonisseur De La Bath , '; ?>
+    <p><?php echo 'Bonjour'. $name .'comment est votre blanquette ?'; ?></p>
+</body>
+</html>
